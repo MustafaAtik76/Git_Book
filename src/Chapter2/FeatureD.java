4c9323a957aaabcfba1e28a6f6d08c078055da44
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-15 17:49:30 +0300

    FeatureD finished

---
+++ src/Chapter2/FeatureD.java
@@ -0,0 +1,8 @@
+package Chapter2;
+
+public class FeatureD {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello world");
+    }
+}
